8b5237bd00d3d56b7a21536c704bb299635fcab3
Sophia Shanthan <100741746+SophShan@users.noreply.github.com>
2024-05-29 13:03:42 -0400

    expand function

---
+++ faqSection/javascript.js
@@ -0,0 +1,9 @@
+
+function expand (id){
+  if (document.getElementById (id).style.display == 'none') {
+    document.getElementById (id).style.display = '';
+  }
+  else {
+    document.getElementById (id).style.display = 'none';
+  }
+}
